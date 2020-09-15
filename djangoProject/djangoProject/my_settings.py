5b5338205c60e2cb4f8cbdd71411de14b001eb26
kanghanna1202 <namnam1178@daum.net>
2020-09-15 20:33:20 +0900

    Apply gitignore

---
+++ djangoProject/djangoProject/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_login',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
