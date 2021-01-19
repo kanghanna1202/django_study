78edaacdac7e3cacc09ccae699b72937923fd968
kanghanna1202 <namnam1178@daum.net>
2021-01-19 17:25:15 +0900

    [FIX] Fix AbstractUser SystemCheckError - REQUIRED_FIELD

--- drf_jwt_refreshtoken/jwtauth/models.py
+++ drf_jwt_refreshtoken/jwtauth/models.py
@@ -52,4 +52,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['nickname', 'password']
