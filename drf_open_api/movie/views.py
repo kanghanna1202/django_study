76882d4bd03d4ae08bedb1fa649a9f21f935f1bf
hanna <namnam1178@daum.net>
2021-03-22 19:17:32 +0900

    [ADD] Create movie search view

--- drf_open_api/movie/views.py
+++ drf_open_api/movie/views.py
@@ -12,10 +12,34 @@ from drf_open_api import my_settings
 @csrf_exempt
 @api_view(['GET'])
 def movie_list(request):
+
     ServiceKey = my_settings.SERVICE_KEY
 
     url = "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2"\
           + "&ServiceKey=" + ServiceKey + "&listCount=3"
+
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+
+    rescode = response.getcode()
+
+    if rescode == 200:
+        response_body = response.read()
+        dict = json.loads(response_body.decode('utf-8'))
+        return Response(dict, status=status.HTTP_200_OK)
+    else:
+        return Response(rescode, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['POST'])
+def search_movie(request):
+
+    ServiceKey = my_settings.SERVICE_KEY
+    search = urllib.parse.quote(request.title)
+
+    url = "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2" \
+          + "&ServiceKey=" + ServiceKey + "&query" + search
 
     request = urllib.request.Request(url)
     response = urllib.request.urlopen(request)
