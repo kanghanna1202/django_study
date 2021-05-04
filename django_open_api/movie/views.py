d6b4e869ab3f007a5d8fac770e350cd5571dc56b
hanna <namnam1178@daum.net>
2021-05-04 15:29:39 +0900

    [ADD] Get data from open api

--- django_open_api/movie/views.py
+++ django_open_api/movie/views.py
@@ -1,22 +1,31 @@
-import json
-import urllib
+from urllib.parse import urlencode, quote_plus, unquote
 
-from django.http import HttpResponse, JsonResponse
+import bs4
+import requests
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django_open_api import my_settings
 
 
+@api_view(['GET'])
 def movie_list(self):
     service_key = my_settings.SERVICE_KEY
-    url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json" \
-          + "?key=" + service_key
 
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
+    url = 'http://open.kmrb.or.kr/openapi-data/service/MvResultService/mvResult'
 
-    rescode = response.getcode()
+    query_params = '?' + urlencode({quote_plus('ServiceKey'): service_key,
+                                   quote_plus('pageNo'): '1', quote_plus('numOfRows'): '2', quote_plus('title'): '1987',
+                                   quote_plus('rtNo'): '2017-MF02149', quote_plus('aplcName'): '주식회사 우정필름',
+                                   quote_plus('stDate'): '20171201', quote_plus('edDate'): '20171231'})
 
-    if rescode == 200:
-        response_body = response.read()
-        dict = json.loads(response_body.decode('utf-8'))['movieListResult']['movieList']
-        return HttpResponse(status=201)
-    else:
-        return JsonResponse({"message": rescode}, status=400)
+    get_data = requests.get(url + unquote(query_params))
+    soup = bs4.BeautifulSoup(get_data.content, 'html.parser')
+
+    data = soup.find_all('items')
+
+    for i in data:
+        print(data)
+
+    return Response(status=status.HTTP_200_OK)
